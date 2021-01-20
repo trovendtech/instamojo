1fdd43b8ebd16cb395a9458a2525e27c85e90804
Trovend Technology Pvt Ltd <64475555+trovend@users.noreply.github.com>
2021-01-20 14:59:50 +0530

    First Version with Token Generation till Order ID for Mobile SDKs

---
+++ instamojo.js
@@ -0,0 +1,182 @@
+var request = require('request');
+// const axios = require('axios').default;
+
+var HOSTS = {
+  'production': "https://api.instamojo.com/v2/",
+  'test': "https://test.instamojo.com/v2/"
+};
+
+var API = {
+  'createPayment': 'payment_requests/',
+  'payments': 'payments/',
+  'paymentStatus': 'payment_requests/',
+  'refunds': 'payments/',
+  'paymentforSDK': 'gateway/orders/payment-request/',
+};
+
+module.exports = {
+  CURRENT_TOKEN: '',
+  CURRENT_HOST: 'production',
+
+  AuthHEADERS: {
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Authorization': ""
+  },
+
+  isSandboxMode: function (isSandbox) {
+    if (isSandbox) {
+      this.CURRENT_HOST = 'test';
+    } else {
+      this.CURRENT_HOST = 'production';
+    }
+  },
+
+  setToken: function (resToken) {
+    this.CURRENT_TOKEN = resToken;
+  },
+
+  createPayment: function (data, callback) {
+    request.post({
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer ' + this.CURRENT_TOKEN },
+      url:  HOSTS[this.CURRENT_HOST] + API.createPayment,
+      form: data
+    }, function (error, response, body) {
+      var result = body;
+      callback(error, result);
+    });
+  },
+
+  createPaymentforSDK: function (data, callback) {
+    request.post({
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer ' + this.CURRENT_TOKEN },
+      url:  HOSTS[this.CURRENT_HOST] + API.paymentforSDK,
+      form: data
+    }, function (error, response, body) {
+      var result = body;
+      callback(error, result);
+    });
+  },
+
+  seeAllLinks: function (callback) {
+    request.get({
+      url: HOSTS[this.CURRENT_HOST] + API.links
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  getAllPaymentRequests: function (callback) {
+    request.get({
+      // headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.paymentStatus
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  getPaymentRequestStatus: function (id, callback) {
+    request.get({
+      //   headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.paymentStatus + id + '/'
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  getPaymentDetails: function (payment_request_id, payment_id, callback) {
+    request.get({
+      //    headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.paymentStatus + payment_request_id + '/' + payment_id + '/'
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  createRefund: function (refundRequest, callback) {
+    request.post({
+      //   headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.refunds + '/',
+      form: refundRequest
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  getAllRefunds: function (callback) {
+    request.get({
+      //   headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.refunds
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  getRefundDetails: function (id, callback) {
+    request.get({
+      //   headers: this.HEADERS,
+      url: HOSTS[this.CURRENT_HOST] + API.refunds + id + '/'
+    }, function (error, response, body) {
+      var result = JSON.parse(body);
+      callback(error, result);
+    });
+  },
+
+  PaymentData: function () {
+    return ({
+      'purpose': '', // required
+      'amount': 0,  // required
+      'currency': 'INR',
+      'buyer_name': '',
+      'email': '',
+      'phone': '',
+      'send_email': '',
+      'send_sms': '',
+      'allow_repeated_payments': '',
+      'webhook': '',
+      'redirect_url': '',
+
+      setWebhook: function (hook) {
+        this.webhook = hook;
+      },
+
+      setRedirectUrl: function (redirectUrl) {
+        this.redirect_url = redirectUrl;
+      }
+    });
+  },
+
+  OrderData: function () {
+    return ({
+      'id': '', // required
+      'phone': ''
+    });
+  },
+
+  AuthTokenRequestData: function () {
+    return ({
+      'grant_type': '',
+      'client_id': '',
+      'client_secret': ''
+    });
+  },
+
+  RefundRequest: function () {
+    return ({
+      'payment_id': '',
+      'type': '',  // Available : ['RFD', 'TNR', 'QFL', 'QNR', 'EWN', 'TAN', 'PTH']
+      'body': '',
+
+      setRefundAmount: function (refundAmount) {
+        this.refund_amount = refundAmount;
+      }
+    });
+  }
+};
